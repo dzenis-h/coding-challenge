a95656cdbb96473b29f8a08bea308b6d948c7f8d
Dzenis <dzenis.hankusic@gmail.com>
2022-03-29 20:54:20 +0200

    Add script

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./css/index.css";
-
 import App from "./App";
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdfXHKLKERbWkVIcr6NZQUVb6oSHVJisM",
+  authDomain: "coding-challenge-2022.firebaseapp.com",
+  projectId: "coding-challenge-2022",
+  storageBucket: "coding-challenge-2022.appspot.com",
+  messagingSenderId: "150496575059",
+  appId: "1:150496575059:web:760f00612e969cd7d5d0a6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
